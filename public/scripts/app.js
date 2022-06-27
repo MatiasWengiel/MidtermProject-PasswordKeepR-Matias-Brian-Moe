626c4a391990993b99dd22daea34cd74c7bda2ad
Mohammed <MMD.Mahdi21@gmail.com>
2022-06-27 22:01:17 +0000

    add new feature (add-password)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,4 +7,24 @@ $(document).ready(function() {
     $('.display-password').hide(); //Hides any passwords currently on display
     $(`.${passwordNickname}`).slideDown("slow"); //Slides down the selected password
   })
+
+  $("#show-form").on('click', () =>{
+    $(".new-password").removeClass("hidden");
+  })
+
+  $(".new-password-form").on("submit", function(event){
+    event.preventDefault();
+    console.log("submit password")
+    const password = $(this).find("input").val()
+
+    
+    // const data = {password};
+    const data = $(this).serialize();
+    console.log(data);
+    $.post("/api/passwords", data)
+    .then(res =>{
+      console.log(res) // Adding new password to password
+    })
+  })
+
 })
