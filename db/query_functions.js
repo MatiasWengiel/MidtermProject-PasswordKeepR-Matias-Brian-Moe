3a44788b7b6733144e60d68ee3402208bac1e9bd
Matias Wengiel <matiaswengiel@gmail.com>
2022-06-27 11:13:48 -0500

    Comment out currently unused DB queries. TODO: Delete before final submission

--- db/query_functions.js
+++ db/query_functions.js
@@ -9,29 +9,29 @@ const getAllOrganizationalPasswords = (orgId) => {
   `
 }
 
-const getAllOrganizationalPasswordsWithinCategory = (orgId, category) => {
-  return `
-  SELECT website_nickname, login_email, login_password FROM passwords
-  JOIN organizations ON passwords.organization_id = organizations.id
-  JOIN categories ON passwords.category_id = categories.id
-  WHERE passwords.organization_id = ${orgId} AND categories.id = ${category};
-  ` // Still need to change categories.id to fetch the id from the user selection
-}
+// const getAllOrganizationalPasswordsWithinCategory = (orgId, category) => {
+//   return `
+//   SELECT website_nickname, login_email, login_password FROM passwords
+//   JOIN organizations ON passwords.organization_id = organizations.id
+//   JOIN categories ON passwords.category_id = categories.id
+//   WHERE passwords.organization_id = ${orgId} AND categories.id = ${category};
+//   ` // Still need to change categories.id to fetch the id from the user selection
+// }
 
 
-const getAllOrganizationalPasswordsFromSearch = (orgId, query) => {
-  return `
-  SELECT website_nickname, login_email, login_password FROM passwords
-  JOIN organizations ON passwords.organization_id = organizations.id
-  JOIN categories ON passwords.category_id = categories.id
-  WHERE passwords.website_nickname LIKE '%${query}%' OR
-        passwords.website_url LIKE '%${query}%' OR
-        categories.category LIKE '%${query}%' AND
-        passwords.organization_id = ${orgId};;
-  `
-}
+// const getAllOrganizationalPasswordsFromSearch = (orgId, query) => {
+//   return `
+//   SELECT website_nickname, login_email, login_password FROM passwords
+//   JOIN organizations ON passwords.organization_id = organizations.id
+//   JOIN categories ON passwords.category_id = categories.id
+//   WHERE passwords.website_nickname LIKE '%${query}%' OR
+//         passwords.website_url LIKE '%${query}%' OR
+//         categories.category LIKE '%${query}%' AND
+//         passwords.organization_id = ${orgId};;
+//   `
+// }
 module.exports = {
   getAllOrganizationalPasswords,
-  getAllOrganizationalPasswordsWithinCategory,
-  getAllOrganizationalPasswordsFromSearch
+  // getAllOrganizationalPasswordsWithinCategory,
+  // getAllOrganizationalPasswordsFromSearch
 }
