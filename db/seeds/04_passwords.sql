ceb515acaeb02ab4ff0fccec06c2559a53849ba4
Matias Wengiel <matiaswengiel@gmail.com>
2022-06-27 11:11:25 -0500

    Fix typo on github url

--- db/seeds/04_passwords.sql
+++ db/seeds/04_passwords.sql
@@ -1,6 +1,6 @@
 -- passwords table seeds
 INSERT INTO passwords (login_email, login_password, website_nickname, website_url, category_id, organization_id) VALUES ('myricoh@ricohcanada.ca', 'ricoh01', 'Office 365', 'https://www.office.com/,', 3, 1);
-INSERT INTO passwords (login_email, login_password, website_nickname, website_url, category_id, organization_id) VALUES ('myricoh@github.com', 'ricoh02', 'Github', 'https://www.github.com/,', 1, 1);
+INSERT INTO passwords (login_email, login_password, website_nickname, website_url, category_id, organization_id) VALUES ('myricoh@github.com', 'ricoh02', 'Github', 'https://www.github.com/', 1, 1);
 INSERT INTO passwords (login_email, login_password, website_nickname, website_url, category_id, organization_id) VALUES ('myricoh@gmail.com', 'ricoh3', 'Instagram', 'https://www.instagram.com', 1, 1);
 INSERT INTO passwords (login_email, login_password, website_nickname, website_url, category_id, organization_id) VALUES ('myricoh@gmail.com', 'ricoh4', 'Staples', 'https://www.staples.ca', 2, 1);
 INSERT INTO passwords (login_email, login_password, website_nickname, website_url, category_id, organization_id) VALUES ('mywalmart@walmart.ca', 'walmart01', 'Toner Supplies', 'https://www.ricoh.ca/en/products/supplies', 2, 2);
