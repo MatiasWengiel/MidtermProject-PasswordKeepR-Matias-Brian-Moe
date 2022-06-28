19d731d1f424608a43466a4e67c0a15a59c8116c
Matias Wengiel <matiaswengiel@gmail.com>
2022-06-28 11:54:25 -0500

    Modify copy text button to use Clipboard API (current standard)

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,29 +1,13 @@
 $(document).ready(function() {
-
-
+  console.log('helpers is loaded')
 
 // Copy the text in username & password fields
 
-let copyText = document.querySelector("#user-name");
-let copyPassword = document.querySelector("#password");
-
-const handleClickEvent = function(arr) {
-  let input = arr.querySelector("input.text");
-  input.select();
-  document.execCommand("copy");
-  arr.classList.add("active");
-  window.getSelection().removeAllRanges();
-  setTimeout(function () {
-    arr.classList.remove("active");
-  }, 2500);
-}
-
-copyText.querySelector("button").addEventListener("click", function () {
-  handleClickEvent(copyText)
-});
-
-copyPassword.querySelector("button").addEventListener("click", function () {
-  handleClickEvent(copyPassword);
-});
-
+  $('.copy-btn').click(function() {
+    navigator.clipboard.writeText($(this).siblings().text())
+    .then( () => {
+      $('.fa-check').removeClass("fa-check").addClass("fa-copy")
+      $(this).children().removeClass("fa-copy").addClass("fa-check")
+    })
+  })
 });
