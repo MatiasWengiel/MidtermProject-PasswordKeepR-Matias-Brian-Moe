cbd27ee50d2cf5974da6dfb3535934150570d845
Mohammed <MMD.Mahdi21@gmail.com>
2022-06-29 21:29:04 +0000

    debug add password feature

--- routes/passwords.js
+++ routes/passwords.js
@@ -47,6 +47,36 @@ module.exports = (db) => {
       });
   });
 
+  // router.get("/json", (req, res) => {
+  //   const userId = req.session.userId;
+  //   const orgId = req.session.orgId;
+
+  //   db.query(getEmail(orgId))
+  //     .then((data) => {
+  //       const email = data.rows[0].email;
+
+  //       db.query(getOrganization(orgId)).then((data) => {
+  //         const organization = data.rows[0].name;
+
+  //         db.query(getAllOrganizationalPasswords(orgId)).then((data) => {
+  //           const passwords = data.rows;
+
+  //           let templateVars = {
+  //             passwords,
+  //             email,
+  //             organization,
+  //           };
+
+  // //            res.render("passwords_page", templateVars); //We can change this to render the page showing the PWs
+  //           res.json(templateVars);
+  //         });
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       res.status(500).json({ error: err.message });
+  //     });
+  // });
+
 
 
 
@@ -59,16 +89,12 @@ module.exports = (db) => {
 
   router.post("/", (req, res) => {
 
-    console.log("This is line 57 in passwords", req.body)
-    console.log("This is line 58 in passwords", req.session)
-
 
     const { userId, orgId } = req.session;
     const { loginEmail, newPassword, account, url, category } = req.body
     const { email, password, label } = req.body;
     db.query(addNewPassword(), [loginEmail, newPassword, account, url, category, orgId])
-    //console.log(addNewPassword);
-    //db.query(editLogin(email, password, orgId, label));
+    db.query(editLogin(email, password, orgId, label));
 
     res.send(`<h1>You have successfully POSTed to create a new password</h1>`);
   });
